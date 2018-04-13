46e52f55a515ab6176759409714ad1025b89df56
Jacson Curtis <Jacson.Curtis@ideagen.com>
2018-04-13 14:43:45 +0100

    can connect to db & created User Class

--- Bookish/Bookish/Program.cs
+++ Bookish/Bookish/Program.cs
@@ -1,15 +1,28 @@
-﻿using System;
+﻿using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Dapper;
 
 namespace Bookish
 {
     class Program
     {
+        static IDbConnection db = new SqlConnection(ConfigurationManager.ConnectionStrings["BookishConnection"].ConnectionString);
+
         static void Main(string[] args)
         {
+
+            string SqlString = "SELECT TOP 100 [UserId],[Name] FROM [User]";
+            var Users = (List<User>)db.Query<User>(SqlString);
+            foreach (var user in Users)
+            {
+                Console.WriteLine(user.Name + " " + user.UserId);
+            }
+
+            Console.ReadLine();
+
         }
     }
 }
